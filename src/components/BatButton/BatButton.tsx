54bae040555e1f9352e78113db34cdfa485e4009
Alefy00 <alefy_sx@hotmail.com>
2024-06-18 13:19:28 -0300

    gerador de senhas

---
+++ src/components/BatButton/BatButton.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Text, Pressable } from "react-native";
+import * as Clipboard from 'expo-clipboard'
+import { styles } from "./BatButtonStyle";
+import { BatTextInput } from "../BatTextInput/BatTextInput";
+import generatePass from "../../services/passwordService";
+
+
+export function BatButton() {
+  const [pass, setPass] = useState("");
+
+  function handleGenerateButton() {
+    let generateToken = generatePass()
+    setPass(generateToken);
+  }
+  function handleCopyButton(){
+    Clipboard.setStringAsync(pass)
+  }
+  return (
+    <>
+      <BatTextInput pass={pass} />
+      <Pressable style={styles.button} onPress={handleGenerateButton}>
+        <Text style={styles.text}>GENERATE</Text>
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={handleCopyButton}>
+        <Text style={styles.text}>✔ COPY</Text>
+      </Pressable>
+    </>
+  );
+}
